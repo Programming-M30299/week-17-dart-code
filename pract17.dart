241e0bec57930ae4dc575cc2db0dd018a597a68d
manighahrmani <mashapicasso@gmail.com>
2024-02-11 03:42:33 +0000

    Add higher order examples to pract

--- pract17.dart
+++ pract17.dart
@@ -1,3 +1,20 @@
 void main() {
   print('Hello, World!');
+  List<int> numbers = [1, 2, 3, 4, 5];
+  var squares = numbers.map((number) => number * number);
+  print(squares.toList()); // Output: [1, 4, 9, 16, 25]
+
+  numbers = [1, 2, 3, 4, 5];
+  var sum = numbers.reduce((value, element) => value + element);
+  print(sum); // Output: 15
+
+  Set<int> numbersSet = {1, 2, 3, 4, 5};
+  var evenNumbers = numbersSet.where((number) => number % 2 == 0);
+  print(evenNumbers.toSet()); // Output: {2, 4}
+
+  Map<String, int> ages = {'Alice': 20, 'Bob': 25, 'Charlie': 30};
+  ages.forEach((key, value) => print('$key is $value years old'));
+  // Output: Alice is 20 years old
+  //         Bob is 25 years old
+  //         Charlie is 30 years old
 }
