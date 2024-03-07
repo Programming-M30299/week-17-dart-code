0186607dabc359e5ba8c10041bc33bd323cb0a84
manighahrmani <mashapicasso@gmail.com>
2024-03-07 21:23:20 +0000

    Update lect17.dart

--- lect17.dart
+++ lect17.dart
@@ -1,31 +1,35 @@
-// void main() {
-//   String fruits = '🍎,🍌:🍇,🍊;🍐';
-//   int index = fruits.indexOf('🍇');
-//   String untilGrape = fruits.substring(0, index);
-//   print(untilGrape); // 🍎,🍌:
-
-//   List<String> list = fruits.split(',');
-//   print(list); // ['🍎', '🍌:🍇', '🍊;🍐']
-//   list = fruits.split(RegExp(r'[:;,]+'));
-//   print(list); // ['🍎', '🍌', '🍇', '🍊', '🍐']
-
-//   List<String> colours = ['🔵', '⚪', '🔴'];
-//   String frenchFlag = colours.join('');
-//   print(frenchFlag); // 🔵⚪🔴
-// }
-
-bool isValidEmail(String email) {
-  if (!email.contains('@'))
-    return false;
-  else if (!email.endsWith('port.ac.uk'))
-    return false;
-  else
-    return true;
-}
-
 void main() {
+  String fruits = '🍎,🍌:🍇,🍊;🍐';
+  int index = fruits.indexOf('🍇');
+  String untilGrape = fruits.substring(0, index);
+  print(untilGrape); // 🍎,🍌:
+
+  List<String> list = fruits.split(',');
+  print(list); // ['🍎', '🍌:🍇', '🍊;🍐']
+  list = fruits.split(RegExp(r'[:;,]+'));
+  print(list); // ['🍎', '🍌', '🍇', '🍊', '🍐']
+
+  List<String> colours = ['🔵', '⚪', '🔴'];
+  String frenchFlag = colours.join('');
+  print(frenchFlag); // 🔵⚪🔴
+
   print(isValidEmail('matthew.poole@port.ac.uk')); // true
   print(isValidEmail('up832240@myport.ac.uk')); // true
-  // Now bad examples
   print(isValidEmail('john.doe@example.com')); // false
+}
+
+// bool isValidEmail(String email) {
+//   if (!email.contains('@'))
+//     return false;
+//   else if (!email.endsWith('port.ac.uk'))
+//     return false;
+//   else
+//     return true;
+// }
+
+bool isValidEmail(String email) =>
+    email.contains("@") && email.endsWith("port.ac.uk");
+
+bool isValidStudentEmail(String email) {
+  
 }
